73cc811dd65fd2d9989f10427bf9d9557ee94dd4
enzoevn <viacava.enzo@gmail.com>
2024-09-05 14:40:37 +0000

    mod install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo mv vision_utilities /vision_utilities_temp
-sudo rm -r vision_utilities
-sudo mv vision_utilities_temp vision_utilities
+sudo mv vision_utilities/ ../vision_utilities_temp
+sudo rm -r vision_utilities/
+sudo mv vision_utilities_temp/ vision_utilities
